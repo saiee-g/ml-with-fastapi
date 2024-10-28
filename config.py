3cda25cfc113da76af40bb90f07538f6a594d77c
saiee-g <your@example.comgulhanesaiee22@gmail.com>
2024-10-28 17:30:43 +0530

    path db path modified

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:Saiee%40123@localhost:5432/titanic"
+DATABASE_URL = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(DATABASE_URL)
 
